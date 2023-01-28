07b00c83f710efe54083f65b7a1ac7a035a7df74
Pavel_Pichugin <pichps@mail.ru>
2023-01-28 12:20:23 +0300

    save1

---
+++ exemple006_name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+Console.WriteLine("УРА! Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
