4158560099d493bd6393d19642b034b7ac89941a
Pavel_Pichugin <pichps@mail.ru>
2023-01-28 12:02:40 +0300

    save1

---
+++ exemple005_random/Program.cs
@@ -0,0 +1,7 @@
+﻿int NumberA = new Random().Next(1,10); // 1, 2, 3....9
+Console.WriteLine (NumberA);
+int NumberB = new Random().Next(1,10); // 1, 2, 3....9
+Console.WriteLine (NumberB);
+int Result = NumberA + NumberB;
+Console.WriteLine (Result);
+
