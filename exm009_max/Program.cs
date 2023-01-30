96cda8e207b1b60321ddca45c163634ba9e9f022
Pavel_Pichugin <pichps@mail.ru>
2023-01-30 06:54:53 +0300

    finish-exm009

--- exm009_max/Program.cs
+++ exm009_max/Program.cs
@@ -1,17 +1,17 @@
-﻿int Max(int arg1, int arg2, int arg3);
+﻿int Max(int arg1, int arg2, int arg3)
 {
-    int result = arg1;
-    if (arg2 > result) result = arg2;
-    if (arg3 > result) result = arg3;
-    return result;
+int result = arg1;
+if(arg2>result) result=arg2;
+if(arg3>result) result=arg3;
+return result;
 }
 
-int a1 = 5, b1 = 4, c1 = 20, a2 = 89, b2 = 45, c2 = 78, a3 = 7, b3 = 2, c3 = 18;
+int a1 = 5, b1 = 4, c1 = 20, a2 = 89, b2 = 45, c2 = 781, a3 = 7, b3 = 2, c3 = 18;
 
-int max1 = Max (a1, b1, c1);
+int max1 = Max(a1, b1, c1);
 int max2 = Max (a2, b2, c2);
 int max3 = Max (a3, b3, c3);
 
-int max = Max(max1, max2, max3);
+int max = Max (max1, max2, max3);
 
 Console.WriteLine(max);
