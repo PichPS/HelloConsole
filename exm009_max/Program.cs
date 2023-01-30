493430a3dc49e73b83d87c5ef61d0648e83bc6b8
Pavel_Pichugin <pichps@mail.ru>
2023-01-30 08:34:00 +0300

    finis2-exm009-s massivom

--- exm009_max/Program.cs
+++ exm009_max/Program.cs
@@ -1,4 +1,6 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿int [] array = {11,21,31,41,15,61,17,18,169};
+
+int Max(int arg1, int arg2, int arg3)
 {
 int result = arg1;
 if(arg2>result) result=arg2;
@@ -6,12 +8,9 @@ if(arg3>result) result=arg3;
 return result;
 }
 
-int a1 = 5, b1 = 4, c1 = 20, a2 = 89, b2 = 45, c2 = 781, a3 = 7, b3 = 2, c3 = 18;
-
-int max1 = Max(a1, b1, c1);
-int max2 = Max (a2, b2, c2);
-int max3 = Max (a3, b3, c3);
-
-int max = Max (max1, max2, max3);
-
-Console.WriteLine(max);
+int result = Max (
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8])
+);
+Console.WriteLine($"наибольшее число - {result}");
