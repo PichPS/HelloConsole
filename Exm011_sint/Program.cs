5663da0a05e75e768568845ef1395276b3ac539a
Pavel_Pichugin <pichps@mail.ru>
2023-01-30 09:15:25 +0300

    exm011-1

---
+++ Exm011_sint/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[10];
+
+void FillArray(int[] collection)
+{
+int length = collection.Length;
+int index = 0;
+while (index < length)
+{
+    collection[index] = new Random().Next(1, 10);
+    index++;
+    }
+}
+
