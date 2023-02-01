ecab9679302a9398f0e2b35ca37d715bf5367f97
Pavel_Pichugin <pichps@mail.ru>
2023-02-01 15:50:29 +0300

    exmp12 - код не работает

---
+++ exm012/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] collection)
+{
+int length = collection.Length;
+int index = 0;
+while (index < length)
+
+{
+collection[index] = new Random().Next(1, 10);
+index++;
+}
+}
+
+void PrintArray(int[] col);
+
+int count = col.Length;
+int position = 0;
+
+while (position < count)
+{
+Console.WriteLine(col[position]);
+position++;
+}
+
+FillArray(array);
+PrintArray(array);
